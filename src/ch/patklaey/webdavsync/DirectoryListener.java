9cdf7e102c210e51ad3f8586c16d3e713c84e288
patklaey <pat.klaey@unifr.ch>
2015-05-17 19:56:46 +0200

    Using Service instead of IntentService

--- src/ch/patklaey/webdavsync/DirectoryListener.java
+++ src/ch/patklaey/webdavsync/DirectoryListener.java
@@ -1,29 +1,20 @@
 package ch.patklaey.webdavsync;
 
-import android.app.IntentService;
+import android.app.Service;
 import android.content.Intent;
+import android.os.IBinder;
 import android.util.Log;
 import android.widget.Toast;
 
-/**
- * Created by uni on 4/16/15.
- */
-public class DirectoryListener extends IntentService {
-
-    public static final int START = 1;
-    public static final int STOP = 2;
-    private boolean running = true;
-
-    /**
-     * Creates an IntentService.  Invoked by your subclass's constructor.
-     */
-    public DirectoryListener() {
-        super("DirectoryListener");
-    }
-
+public class DirectoryListener extends Service {
 
     @Override
-    protected void onHandleIntent(Intent intent) {
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
         Toast.makeText(this, "service starting", Toast.LENGTH_SHORT).show();
 
         String directory = intent.getStringExtra(MainActivity.EXTRA_DIRECTORY_TO_OBSERVE);
@@ -33,14 +24,7 @@ public class DirectoryListener extends IntentService {
         observer.startWatching();
         Log.d("Service", "Observing " + directory);
 
-        while (true) {
-            try {
-                Thread.sleep(10000);
-                Log.d("Service", "Still running");
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-        }
+        return START_STICKY;
     }
 
 }
